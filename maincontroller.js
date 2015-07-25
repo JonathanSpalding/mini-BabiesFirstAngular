a02abf66f1e40d4f5c18add818c175beb2354268
Jon Spalding <jon.spalding@hotmail.com>
2015-07-25 15:51:05 -0600

    First push for Project

---
+++ maincontroller.js
@@ -0,0 +1,11 @@
+// var app = angular.module("friendsList");
+
+// app.controller("mainCtrl", function($scope) {
+// 	$scope.friends = ['Devin Hoole', 'Colton Anderson', 'Afton Despain'];
+// });
+
+var app = angular.module('friendsList');
+
+app.controller('mainCtrl', function($scope){
+$scope.friends = ['Devin Hoole', 'Colton Anderson', 'Afton Despain'];
+});
